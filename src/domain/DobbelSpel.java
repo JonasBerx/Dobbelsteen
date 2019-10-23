5a7c1d3517a22c1ff61267de43226794c584ba44
Pieter Fiers <Ubipo.Skippy@gmail.com>
2019-10-23 16:13:55 +0200

    Optimised remove and update

--- src/domain/DobbelSpel.java
+++ src/domain/DobbelSpel.java
@@ -14,19 +14,11 @@ public class DobbelSpel implements Observable {
 
     @Override
     public void removeObserver(Observer observer) {
-        for (int i = 0; i < observers.size(); i++) {
-            if (observer != null) {
-                if (observers.get(i) == observer) {
-                    observers.remove(observer);
-                }
-            }
-        }
+        observers.remove(observer);
     }
 
     @Override
     public void notifyObserver(Score score, Player player) {
-        for (int i = 0; i < observers.size(); i++) {
-            observers.get(i).update(player, score);
-        }
+        observers.forEach(o -> o.update(player, score));
     }
 }
